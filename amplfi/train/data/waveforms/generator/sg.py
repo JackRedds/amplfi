dd35b25729e09a3fbfb9f0f4165d96748cb3029f
JackRedds <Redep028@umn.edu>
2026-03-15 14:32:11 -0500

    changing multi sg generator

--- amplfi/train/data/waveforms/generator/sg.py
+++ amplfi/train/data/waveforms/generator/sg.py
@@ -1,5 +1,5 @@
 import torch
-from ml4gw.waveforms import SineGaussian
+from ml4gw.waveforms import SineGaussian, MultiSineGaussian
 from typing import Tuple, Dict
 from typing import TYPE_CHECKING, Optional
 if TYPE_CHECKING:
@@ -25,41 +25,9 @@ class SGGenerator(WaveformGenerator):
 
 
 class MultiSGGenerator(WaveformGenerator):
-    def __init__(self, max_shift: float = 1e-2, *args, **kwargs):
+    def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.multi_sg = SineGaussian(self.sample_rate, self.duration)
-        self.max_shift = max_shift
-
-    def ave_parameters(self, parameters: Dict[str, torch.Tensor]) -> Dict[str, torch.Tensor]:
-        averaged_params = {}
-        for i, params in parameters.items():
-            for k, v in params.items():
-                if k not in averaged_params:
-                    averaged_params[k] = []
-                averaged_params[k].append(v.mean(dim=0))
-        # average parameters
-        for k in averaged_params:
-            averaged_params[k] = torch.stack(averaged_params[k])
-        return averaged_params
-
-    def shift_waveforms(self, cross: torch.Tensor, plus: torch.Tensor) -> Tuple[torch.Tensor, torch.Tensor]:
-        N = cross.shape[0]
-        shifts = (torch.rand(N, device=cross.device) - 0.5) * 2 * self.max_shift
-        shift_samples = (shifts * self.sample_rate).long()
-        shifted_cross = torch.zeros_like(cross)
-        shifted_plus = torch.zeros_like(plus)
-        for i in range(N):
-            shift = shift_samples[i].item()
-            if shift > 0:
-                shifted_cross[i, shift:] = cross[i, :-shift]
-                shifted_plus[i, shift:] = plus[i, :-shift]
-            elif shift < 0:
-                shifted_cross[i, :shift] = cross[i, -shift:]
-                shifted_plus[i, :shift] = plus[i, -shift:]
-            else:
-                shifted_cross[i] = cross[i]
-                shifted_plus[i] = plus[i]
-        return shifted_cross, shifted_plus
+        self.multi_sg = MultiSineGaussian(self.sample_rate, self.duration)
 
     def get_val_waveforms(self, _, world_size):
         num_waveforms = self.num_val_waveforms // world_size
@@ -106,16 +74,4 @@ class MultiSGGenerator(WaveformGenerator):
         return waveforms[..., start:stop]
 
     def forward(self, **parameters):
-        cross_waveforms = []
-        plus_waveforms = []
-        for i, params in parameters.items():
-            cross, plus = self.multi_sg(**params)
-            cross, plus = self.shift_waveforms(cross, plus)
-            cross = cross.mean(dim=0, keepdim=True)
-            plus = plus.mean(dim=0, keepdim=True)
-            cross_waveforms.append(cross)
-            plus_waveforms.append(plus)
-        cross = torch.vstack(cross_waveforms)
-        plus = torch.vstack(plus_waveforms)
-
-        return cross, plus
+        return self.multi_sg(**parameters)
